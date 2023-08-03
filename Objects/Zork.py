e0b1dc3f8177663d267fca36160162adf5a34605
Shaghk <25Cheny@mbbc.qld.edu.au>
2023-08-03 21:15:08 +1000

    Update Zork.py
    Nwe

---
+++ Objects/Zork.py
@@ -0,0 +1,12 @@
+from GameFrame import Level, RoomObject
+
+class zork(RoomObject):
+    """
+    A class for the game's antagoist
+    """
+    def __init__(self, room, x, y):
+        """
+        Initialise the Boss object
+        """
+        
+    
